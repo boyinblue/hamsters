15f9df111c9904c823fb68cb42b41754949522e5
boyinblue <boyinblue@naver.com>
2023-11-26 20:07:13 +0900

    Update

--- stop_in_front_of_obj.py
+++ stop_in_front_of_obj.py
@@ -15,19 +15,26 @@ def stop_method1():
             return
 
 def stop_method2():
+    hamster.wheels(100,100)
+    while True:
+        if hamster.left_proximity() > 60 or hamster.right_proximity() > 60:
+            hamster.stop()
+            hamster.sound("beep")
+            break
+
     while True:
         if hamster.left_proximity() > 87 or hamster.right_proximity() > 87:
             hamster.stop()
             hamster.sound("beep")
-            hamster.move_forward(1.8)
+            hamster.move_forward(1.95)
             hamster.note("C4", 0.095)
-            hamster.note("off", 0.005)
+            hamster.note(hamster.NOTE_OFF, 0.005)
             hamster.note("E4", 0.095)
-            hamster.note("off", 0.005)
+            hamster.note(hamster.NOTE_OFF, 0.005)
             hamster.note("G4", 0.095)
-            hamster.note("off", 0.005)
+            hamster.note(hamster.NOTE_OFF, 0.005)
             hamster.note("C5", 0.095)
-            hamster.note("off")
+            hamster.note(hamster.NOTE_OFF)
             return
         hamster.wheels( ( target_proximity - hamster.left_proximity() ), ( target_proximity - hamster.right_proximity() ) )
 
