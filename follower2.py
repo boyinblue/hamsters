bd542649563434da80200c185866fac9ba1ab057
boyinblue <boyinblue@naver.com>
2025-06-18 20:50:56 +0900

    Update

---
+++ follower2.py
@@ -0,0 +1,58 @@
+from roboid import *
+from enum import Enum
+
+class SoundMode(Enum):
+    NULL = 0
+    DARK = 1
+    OBJ_LOST = 2
+
+hamster = HamsterS()
+
+proximity_threshold = 15
+target_proximity = 80
+weight = 2
+last_mode = SoundMode.NULL
+
+def sound(mode):
+    global last_mode
+
+    if last_mode == mode:
+        return
+
+    if mode == SoundMode.NULL:
+#        hamster.sound("beep")
+        hamster.note("C4")
+        wait(50)
+        hamster.note("off", 0.05) # break for the same notes
+        hamster.note("G4")
+        wait(50)
+        hamster.note("off", 0.05) # break for the same notes
+    elif mode == SoundMode.DARK:
+        hamster.sound("robot")
+    elif mode == SoundMode.OBJ_LOST:
+#        hamster.sound("siren")
+        hamster.note("G4")
+        wait(50)
+        hamster.note("off", 0.05) # break for the same notes
+        hamster.note("C4")
+        wait(50)
+        hamster.note("off", 0.05) # break for the same notes
+
+    last_mode = mode
+
+while True:
+    # 근접 센서 값이 너무 작으면 그냥 정지한다.
+    if hamster.left_proximity() < proximity_threshold and hamster.right_proximity() < proximity_threshold:
+#        hamster.stop()
+        hamster.move_forward(5, 100)
+        sound(SoundMode.OBJ_LOST)
+        continue
+
+    # 너무 어두워지면 그냥 정지한다.
+    if hamster.light() < 10:
+        hamster.stop()
+        sound(SoundMode.DARK)
+        continue
+
+    sound(SoundMode.NULL)
+    hamster.wheels( ( target_proximity - hamster.left_proximity() ) * weight, ( target_proximity - hamster.right_proximity() ) * weight )
